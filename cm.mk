b6d07150b4b31879452096d5d4bd74abb9960484
David Hays <Dhays90@gmail.com>
2013-11-24 14:14:20 -0600

    t6vzw: update fingerprint

--- cm.mk
+++ cm.mk
@@ -4,7 +4,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=vzwint_wwe BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/vzwint_wwe/t6whl:4.3/JSS15J/268933.4:user/release-keys" PRIVATE_BUILD_DESC="1.16.651.4 CL268933 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneMaxVZW BUILD_ID=JSS15J BUILD_FINGERPRINT="VERIZON/HTCOneMaxVZW/t6wl:4.3/JSS15J/270175.4:user/release-keys" PRIVATE_BUILD_DESC="1.11.605.4 CL270175 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 PRODUCT_NAME := cm_t6vzw
 PRODUCT_DEVICE := t6vzw
