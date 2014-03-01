0d268e936d86af4c36b0f9bd4501f73ff6567d82
David Hays <Dhays90@gmail.com>
2014-03-01 11:26:12 -0600

    t6vzw: Change inherit order
     * Move comon_full_phone.mk to top. Otherwise default notification
       and alarm tone will be completely wrong
    
    Please see the following changes for an explanation/source:
    
    http://review.cyanogenmod.org/#/c/59655/
    http://review.cyanogenmod.org/#/c/59973/

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
-$(call inherit-product, device/htc/t6vzw/full_t6vzw.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/htc/t6vzw/full_t6vzw.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneMaxVZW BUILD_ID=JSS15J BUILD_FINGERPRINT="VERIZON/HTCOneMaxVZW/t6wl:4.3/JSS15J/270175.4:user/release-keys" PRIVATE_BUILD_DESC="1.11.605.4 CL270175 release-keys"
 
