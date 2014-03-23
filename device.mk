a9a39c93a2f7efbd2470f3432116d1b0576a5ca2
David Hays <Dhays90@gmail.com>
2014-03-23 10:17:06 -0500

    t6vzw: remove uneeded overlay

--- device.mk
+++ device.mk
@@ -17,9 +17,5 @@
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/htc/t6vzw/t6vzw-vendor.mk)
 
-# overlays
-DEVICE_PACKAGE_OVERLAYS += device/htc/t6vzw/overlay
-
-
 # Inherit from t6-common
 $(call inherit-product, device/htc/t6-common/t6-common.mk)
