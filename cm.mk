2cca90c1ec71886ee7ae10f361ad3d9e4141d495
David Hays <dhays90@gmail.com>
2014-09-11 21:17:46 -0500

    t6vzw: add/update props
    Change-Id: I4cb0297c548cfca53e6b62f702595dafbb215d0c

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/t6vzw/full_t6vzw.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneMaxVZW BUILD_ID=KOT49H BUILD_FINGERPRINT="VERIZON/HTCOneMaxVZW/t6wl:4.4.2/KOT49H/333209.1:user/release-keys" PRIVATE_BUILD_DESC="3.08.605.1 CL333209 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneMaxVZW BUILD_ID=KOT49H BUILD_FINGERPRINT="VERIZON/HTCOneMaxVZW/t6wl:4.4.2/KOT49H/333209.2:user/release-keys" PRIVATE_BUILD_DESC="3.09.605.2 CL333209 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
